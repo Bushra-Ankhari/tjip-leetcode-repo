4f2736d47e29b20a512aab67990ad73eda85315b
Bushra-Ankhari <40654094+Bushra-Ankhari@users.noreply.github.com>
2023-01-18 23:00:22 +0600

    Add files via upload

---
+++ week05/Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,23 @@
+///Time Complexity O(N), Space Complexity O(N)
+
+class Solution
+{
+public:
+    TreeNode* searchNode (TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if (root == NULL)
+            return NULL;
+        if (root->val == p->val || root->val ==q->val)
+            return root;
+        auto lres = searchNode (root->left, p, q);
+        auto rres = searchNode (root->right, p,q);
+        if (lres && rres)
+            return root;
+        return lres? lres: rres;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        return searchNode (root, p,q);
+    }
+
+};
