f58536e8b1b36ca3b953df85ae54ad1d6d64bbb1
Bushra-Ankhari <40654094+Bushra-Ankhari@users.noreply.github.com>
2023-01-31 21:56:21 +0600

    Update Kth Smallest Element in a BST.cpp

--- week05/Kth Smallest Element in a BST.cpp
+++ week05/Kth Smallest Element in a BST.cpp
@@ -1,16 +1,20 @@
 ///Time Complexity O(N), Space Complexity O(N)
 
-class Solution {
+class Solution
+{
 public:
-   vector<int> v;
-	void inOrder(TreeNode* root){
-		if(root==NULL)return;
-		inOrder(root->left);
-		v.push_back(root->val);
-		inOrder(root->right);
-	}
-	int kthSmallest(TreeNode* root, int k) {
-		inOrder(root);
-		return v[k-1];
-	}
+    vector<int> v;
+    void inOrder(TreeNode* root)
+    {
+        if(root==NULL)
+            return;
+        inOrder(root->left);
+        v.push_back(root->val);
+        inOrder(root->right);
+    }
+    int kthSmallest(TreeNode* root, int k)
+    {
+        inOrder(root);
+        return v[k-1];
+    }
 };
