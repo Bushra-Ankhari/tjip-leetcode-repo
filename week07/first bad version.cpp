c34e373ab696f4c7440ccd1bb065b96753d6c6f4
Bushra-Ankhari <40654094+Bushra-Ankhari@users.noreply.github.com>
2023-02-06 13:13:36 +0600

    Add files via upload

---
+++ week07/first bad version.cpp
@@ -0,0 +1,24 @@
+///Time Complexity O(logN)
+///Space Complexity O(1)
+
+class Solution
+{
+public:
+    int firstBadVersion(int n)
+    {
+        int left = 1, right = n;
+        while(left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if(isBadVersion(mid))
+            {
+                right=mid;
+            }
+            else
+            {
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+};
