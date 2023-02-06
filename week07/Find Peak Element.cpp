8b93ddeb3f1461da9de1b9fa5e02a36415b29a99
Bushra-Ankhari <40654094+Bushra-Ankhari@users.noreply.github.com>
2023-02-06 15:52:02 +0600

    Add files via upload

---
+++ week07/Find Peak Element.cpp
@@ -0,0 +1,24 @@
+///Time Complexity O(log(N))
+///Space Complexity O(1)
+
+class Solution
+{
+public:
+    int findPeakElement(vector<int>& nums)
+    {
+        int left=0, right= nums.size()-1;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            if (nums[mid]<nums[mid+1])
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid;
+            }
+        }
+        return left;
+    }
+};
